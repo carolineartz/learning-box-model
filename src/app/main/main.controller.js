52619928ed9c5887bba5cf642e4a4aff95b1df25
carolineartz <ceartz@gmail.com>
2015-03-25 14:14:12 -0500

    few code formatting

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -11,12 +11,14 @@ angular.module('cssBoxModel')
             bottom: 20,
             left: 20
         }
+
         $scope.border = {
             top: 15,
             right: 15,
             bottom: 15,
             left: 15
         }
+
         $scope.padding = {
             top: 20,
             right: 20,
@@ -27,6 +29,7 @@ angular.module('cssBoxModel')
         $scope.box = {
             sizing: "content-box"
         }
+
         $scope.dimensions = {
             width: 220,
             height: 220
@@ -37,9 +40,13 @@ angular.module('cssBoxModel')
             height: 220
         }
 
-        $scope.generatedDimensions = {
-            width: 330,
-            height: 330
+        //starts @ content-box: "width" + padding + border =>
+              // width: 220 + 40 + 30 = 290
+              // height: 22 + 40 + 30 = 290
+        //= GENERATED INNER CONTENT BOX DIMENSIONS
+        $scope.generatedContentBoxDimensions = {
+            width: 290,
+            height: 290
         }
 
 
@@ -73,79 +80,81 @@ angular.module('cssBoxModel')
 
         //watch for changes and calculate
         $scope.$watch(function() {
-            $scope.boxPosition.top = $scope.margin.top +
-                $scope.border.top +
-                $scope.padding.top + 6;
 
-            $scope.boxPosition.left = $scope.margin.left +
-                $scope.border.left +
-                $scope.padding.left;
+            $scope.boxPosition.top =      $scope.margin.top +
+                                          $scope.border.top +
+                                          $scope.padding.top + 6;
+
+            $scope.boxPosition.left =     $scope.margin.left +
+                                          $scope.border.left +
+                                          $scope.padding.left;
 
 
             //Margin Styles
-            $scope.styleMargin.width = $scope.margin.right +
-                $scope.margin.left +
-                $scope.border.right +
-                $scope.border.left +
-                $scope.padding.right +
-                $scope.padding.left +
-                getInnerWidth();
+            $scope.styleMargin.width =    $scope.margin.right +
+                                          $scope.margin.left +
+                                          $scope.border.right +
+                                          $scope.border.left +
+                                          $scope.padding.right +
+                                          $scope.padding.left +
+                                          getInnerWidth();
 
-            $scope.styleMargin.height = $scope.margin.top +
-                $scope.margin.bottom +
-                $scope.border.top +
-                $scope.border.bottom +
-                $scope.padding.top +
-                $scope.padding.bottom +
-                getInnerHeight();
+            $scope.styleMargin.height =   $scope.margin.top +
+                                          $scope.margin.bottom +
+                                          $scope.border.top +
+                                          $scope.border.bottom +
+                                          $scope.padding.top +
+                                          $scope.padding.bottom +
+                                          getInnerHeight();
 
-            $scope.styleMargin.top = ($scope.margin.top +
-                $scope.border.top +
-                $scope.padding.top) * -1;
+            $scope.styleMargin.top =     ($scope.margin.top +
+                                          $scope.border.top +
+                                          $scope.padding.top) * -1;
 
-            $scope.styleMargin.left = ($scope.margin.left +
-                $scope.border.left +
-                $scope.padding.left) * -1;
+            $scope.styleMargin.left =    ($scope.margin.left +
+                                          $scope.border.left +
+                                          $scope.padding.left) * -1;
 
 
             //Border Styles
-            $scope.styleBorder.width = $scope.border.right +
-                $scope.border.left +
-                $scope.padding.right +
-                $scope.padding.left +
-                getInnerWidth();
+            $scope.styleBorder.width =    $scope.border.right +
+                                          $scope.border.left +
+                                          $scope.padding.right +
+                                          $scope.padding.left +
+                                          getInnerWidth();
 
-            $scope.styleBorder.height = $scope.border.top +
-                $scope.border.bottom +
-                $scope.padding.top +
-                $scope.padding.bottom +
-                getInnerHeight();
+            $scope.styleBorder.height =   $scope.border.top +
+                                          $scope.border.bottom +
+                                          $scope.padding.top +
+                                          $scope.padding.bottom +
+                                          getInnerHeight();
 
-            $scope.styleBorder.top = ($scope.border.top +
-                $scope.padding.top) * -1;
+            $scope.styleBorder.top =     ($scope.border.top +
+                                          $scope.padding.top) * -1;
 
-            $scope.styleBorder.left = ($scope.border.left +
-                $scope.padding.left) * -1;
+            $scope.styleBorder.left =    ($scope.border.left +
+                                          $scope.padding.left) * -1;
 
 
             //Padding Styles
-            $scope.stylePadding.width = $scope.padding.right +
-                $scope.padding.left +
-                getInnerWidth() + 2;
+            $scope.stylePadding.width =   $scope.padding.right +
+                                          $scope.padding.left +
+                                          getInnerWidth() + 2;
 
-            $scope.stylePadding.height = $scope.padding.top +
-                $scope.padding.bottom +
-                getInnerHeight() + 2;
+            $scope.stylePadding.height =  $scope.padding.top +
+                                          $scope.padding.bottom +
+                                          getInnerHeight() + 2;
 
-            $scope.stylePadding.top = -$scope.padding.top;
+            $scope.stylePadding.top =    -$scope.padding.top;
 
-            $scope.stylePadding.left = -$scope.padding.left;
+            $scope.stylePadding.left =   -$scope.padding.left;
+
             //Inner Content Styles- based on box-sizing
-            $scope.innerContent.width = getInnerWidth();
+            $scope.innerContent.width =  getInnerWidth();
             $scope.innerContent.height = getInnerHeight();
 
-            $scope.generatedDimensions.width = getGeneratedDimensionsWidth();
-            $scope.generatedDimensions.height = getGeneratedDimensionsHeight();
+            $scope.generatedContentBoxDimensions.width = getgeneratedContentBoxDimensionsWidth();
+            $scope.generatedContentBoxDimensions.height = getgeneratedContentBoxDimensionsHeight();
         });
 
         function getInnerWidth() {
@@ -172,7 +181,7 @@ angular.module('cssBoxModel')
             }
         }
 
-        function getGeneratedDimensionsWidth() {
+        function getgeneratedContentBoxDimensionsWidth() {
             if ($scope.box.sizing === 'border-box') {
                 return ($scope.dimensions.width + $scope.margin.right + $scope.margin.left);
             } else {
@@ -186,7 +195,7 @@ angular.module('cssBoxModel')
             }
         }
 
-        function getGeneratedDimensionsHeight() {
+        function getgeneratedContentBoxDimensionsHeight() {
             if ($scope.box.sizing === 'border-box') {
                 return ($scope.dimensions.height + $scope.margin.top + $scope.margin.bottom);
             } else {
